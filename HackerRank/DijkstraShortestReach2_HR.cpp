fd7661401c640cc2047f7259ecca3c31a858b77d
gavinleroy <gavinleroy6@gmail.com>
2019-05-16 18:23:33 -0600

    Completed 100% DijkstrasShortestReach2

--- HackerRank/DijkstraShortestReach2_HR.cpp
+++ HackerRank/DijkstraShortestReach2_HR.cpp
@@ -6,53 +6,46 @@
 using namespace std;
 
 const ll INF = ~(1LL << 63);
+const int I_INF = ~(1 << 31);
 bool* vv = new bool[3001];
 ll* aa = new ll[3001];
 ll** ww = new ll*[3001];
 
 auto cmp = [](const int& l, const int& r) -> bool { return aa[l] < aa[r]; };
 
-void printqueue(priority_queue<int> q)
-{
-	//printing content of queue 
-	while (!q.empty()){
-		printf("%d ", q.top());
-		q.pop();
-	}
-	printf("\n");
-}
-
 void solve_dijkstra(int s, int n){
-	priority_queue<int, vector<int>, decltype(cmp)> qq(cmp);	
+	int ind = 0;
+	int* qq = new int[3001];
+	for(int i = 0; i < 3001; ++i){
+		qq[i] = I_INF;
+	}
 	vv[s] = true;
 	for(int i = 1; i < n+1; ++i){
 		if(ww[s][i] != INF) {
-			qq.push(i); 
+			qq[ind++] = i; 
 			aa[i] = ww[s][i];
 			vv[i] = true;
 		}
 	}
-	while(!qq.empty()){
-		printqueue(qq);
-		int temp =  qq.top();
-		qq.pop();
+	while(ind != 0){
+		sort(qq, qq+ind, cmp);
+		int temp =  qq[0];
+		qq[0] = I_INF;
+		swap(qq[0], qq[ind - 1]);
+		ind--;
 		for(int i = 1; i < n+1; ++i){
 			if(ww[temp][i] != INF){
-		//		if(temp == 14) printf("i is: %d, edgeweight: %lld\n", i, ww[temp][i]);
 				if(aa[i] == -1 || aa[i] > aa[temp] + ww[temp][i]){
-		//			if(temp == 14) printf("i is: %d, currWeight: %lld, node weight: %lld\n", i, aa[i], aa[temp]);	
-					if(i == 14) printf("before: %lld\n", aa[i]);
 					aa[i] = aa[temp] + ww[temp][i];	
-					if(i == 14) printf("after: %lld\n\n", aa[i]);
-		//			if(temp == 14) printf("i is: %d, newWeight: %lld\n\n", i, aa[i]);
 				}
 				if(!vv[i]){
 					vv[i] = true;
-					qq.push(i);
+					qq[ind++] = i;
 				}
 			}
 		}
 	}
+	delete []qq;
 }
 
 int main(void){
@@ -75,7 +68,6 @@ int main(void){
 			int x, y;
 		        ll w;
 			scanf("%d %d %lld", &x, &y, &w);
-			if(x == 11 || y == 11) printf("x is: %d, y is: %d, w is: %lld\n", x, y, w);
 			if(ww[x][y] > w) ww[x][y] = ww[y][x] = w;
 		}
 		scanf("%d", &s);
