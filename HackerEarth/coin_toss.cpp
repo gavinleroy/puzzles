1eb0865cff0a6fc6c7b35eaaeb36ffb840cd2cca
gavinleroy <gavinleroy6@gmail.com>
2019-05-23 20:11:33 -0600

    Solved Coin Toss HE

--- HackerEarth/coin_toss.cpp
+++ HackerEarth/coin_toss.cpp
@@ -7,38 +7,7 @@ using namespace std;
 
 ll MOD = 1e9 + 7;
 
-void _print(ll** aa, int ar, int ac){
-	for(int i = 0; i < ar; ++i){
-		for(int j = 0; j < ac; ++j){
-			printf("%lld ", aa[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
-void exponentiate(ll** aa, ll bb[][3], ll n){
-	ll** temp = new ll*[3];
-	for(int i = 0; i < 3; ++i) temp[i] = new ll[3];
-	ll t = n;
-	while(t--){
-		for(int i = 0; i < 3; ++i){
-			for(int j = 0; j < 3; ++j){
-				temp[i][j] = 0;
-				for(int k = 0; k < 3; ++k){
-					temp[i][j] = (temp[i][j] + (aa[i][k]*bb[k][j]) % MOD) % MOD;
-				}		
-			}
-		}
-		swap(aa, temp);
-		_print(aa, 3, 3);
-		printf("temp:\n");
-		_print(temp, 3, 3);
-	}
-	for(int i = 0; i < 3; ++i) delete []temp[i];
-	delete []temp;
-}
-
-ll** mult_matr(ll** aa, int ar, int ac, ll** bb, int br, int bc){
+void mult_matr(ll** aa, int ar, int ac, ll** bb, int br, int bc){
 	ll** temp = new ll*[ar];
 	for(int i = 0; i < ar; ++i){
 		temp[i] = new ll[bc];
@@ -49,52 +18,57 @@ ll** mult_matr(ll** aa, int ar, int ac, ll** bb, int br, int bc){
 			}
 		}
 	}
-	swap(temp, aa);
-	for(int i = 0; i < ar; ++i) delete []temp[i];
+	for(int i = 0; i < ar; ++i){
+		for(int j = 0; j < bc; ++j){
+			aa[i][j] = temp[i][j];
+		}
+		delete []temp[i];
+	}
 	delete []temp;
-	return aa;
 }
 
-//Create a function that multiplies a matrix by another.
+ll exponentiate(ll** aa, ll** bb, ll n){
+	if(n == 1) return aa[0][0] + aa[0][1];
+
+	exponentiate(aa, bb, n/2LL);
+	mult_matr(aa, 3, 3, aa, 3, 3);
+
+	if(n & 1) mult_matr(aa, 3, 3, bb, 3, 3);
+
+	return aa[0][0] + aa[0][1];
+}
+
 ll solve(ll n){
 	ll** aa = new ll*[3];
 	ll** rr = new ll*[3];
+	ll** bb = new ll*[3];
 	for(int i = 0; i < 3; ++i){
 	       	aa[i] = new ll[3]; 
+		bb[i] = new ll[3];
 		rr[i] = new ll[1];
 	}
-	ll bb[3][3] = { {1, 1, 1}, {1, 0, 0}, {0, 1, 0} };
-	aa[0][0] = 1;
-	aa[0][1] = 1;
-	aa[0][2] = 1;
-	aa[1][0] = 1;
-	aa[1][1] = 0;
-	aa[1][2] = 0;
-	aa[2][0] = 0;
-	aa[2][1] = 1;
-	aa[2][2] = 0;
-//	rr = { {7}, {4}, {2} };
-	rr[0][0] = 4;
-	rr[1][0] = 2;
-	rr[2][0] = 0;
-	exponentiate(aa, bb, n-2);
-	return mult_matr(aa, 3, 3, rr, 3, 1)[0][0];	
+	bb[0][0] = aa[0][0] = 1;
+	bb[0][1] = aa[0][1] = 1;
+	bb[0][2] = aa[0][2] = 1;
+	bb[1][0] = aa[1][0] = 1;
+	bb[1][1] = aa[1][1] = 0;
+	bb[1][2] = aa[1][2] = 0;
+	bb[2][0] = aa[2][0] = 0;
+	bb[2][1] = aa[2][1] = 1;
+	bb[2][2] = aa[2][2] = 0;
+	rr[0][0] = 2;
+	rr[1][0] = 1;
+	rr[2][0] = 1;
+	return exponentiate(aa, bb, n)%MOD;
 }
-
-//Finish solve function that does the work of [[1, 1, 1],^n    [[fn[2] = 7],
-//					       [1, 0, 0],   *   [fn[1] = 4],
-//					       [0, 1, 0]]       [fn[0] = 2]]
-
 
 int main(void){
 	int t;
 	scanf("%d", &t);
-
 	while(t--){
 		ll n;
 		scanf("%lld", &n);
 		printf("%lld\n", solve(n));
 	}		
-
 	return 0;
 }
