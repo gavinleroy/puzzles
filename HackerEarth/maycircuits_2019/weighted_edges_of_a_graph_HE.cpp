0b813c5d0a79b08243cfc3f8623f06a66545f002
gavinleroy <gavinleroy6@gmail.com>
2019-05-29 23:10:46 -0600

    Updated Weighted Edges of a Graph HE

--- HackerEarth/maycircuits_2019/weighted_edges_of_a_graph_HE.cpp
+++ HackerEarth/maycircuits_2019/weighted_edges_of_a_graph_HE.cpp
@@ -1,5 +1,8 @@
 #include <cstdio>
 #include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
 #define MOD 987654319
 #define MAX_N 300001
 
@@ -10,21 +13,25 @@ using namespace std;
 int main(void){
 	ll n, MAX_W, ans = 1;
 	scanf("%lld %lld", &n, &MAX_W);
-	vector<ll>* ee = new vector<ll>[MAX_N];
+	unordered_set<string> ss;
 	ll* mm = new ll[MAX_N];
 	for(int i = 0; i <= n; i++) mm[i] = -1;
 	for(int i = 1; i < n; i++){
 		ll t1, t2, tw;
 		scanf("%lld %lld %lld", &t1, &t2, &tw);
-		ee[t1].push_back(t2);
-		ee[t2].push_back(t1);
+		ss.insert(to_string(min(t1, t2)) + "-" + to_string(max(t1, t2)));
 		if(tw > mm[t1]) mm[t1] = tw;
 		if(tw > mm[t2]) mm[t2] = tw;
 	}	
-	for(int i = 1; i <= n; i++){
-		sort(ee[i].begin(), ee[i].end());
-		//Loop through all the nodes and those that aren't adjacent to the ith node we will count all possibilities. 
+	for(ll i = 1; i <= n; i++){
+		for(ll j = i+1; j <= n; j++){
+			if(ss.find(to_string(i) + "-" + to_string(j)) == ss.end()){
+				ll diff = MAX_W - max(mm[i], mm[j]);
+				ans = (ans * ( (MAX_W - max(mm[i], mm[j])) + 2LL) % MOD ) % MOD;
+			}
+		}
 	}
-	delete []ee;
+	printf("%lld\n", ans);
+	delete []mm;
 	return 0;
 }
