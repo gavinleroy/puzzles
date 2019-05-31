3be11ce03ea94953b17292d0cafa0a9c153f9766
gavinleroy <gavinleroy6@gmail.com>
2019-05-31 17:51:01 -0600

    Improved weighted edges of a graph HE

--- HackerEarth/maycircuits_2019/weighted_edges_of_a_graph_HE.cpp
+++ HackerEarth/maycircuits_2019/weighted_edges_of_a_graph_HE.cpp
@@ -1,8 +1,5 @@
 #include <cstdio>
-#include <vector>
 #include <algorithm>
-#include <string>
-#include <unordered_set>
 #define MOD 987654319
 #define MAX_N 300001
 
@@ -10,28 +7,63 @@ typedef long long ll;
 
 using namespace std;
 
+struct node{
+	ll i = -1;
+	ll w = -1;
+	ll a = 0;
+};
+
+struct edge{
+	ll n1;
+	ll n2;
+	ll w;
+};
+
+ll exp(ll x, ll y, ll p){ 
+    ll res = 1LL;
+    x = x % p;  
+    while (y > 0LL){ 
+        if (y & 1LL) 
+            res = (res*x) % p; 
+        y = y>>1LL;
+        x = (x*x) % p;   
+    } 
+    return res; 
+} 
+
+auto cmp = [](const node a, const node b){
+	if(a.w == b.w) return a.i < b.i;
+	else return a.w < b.w;
+};
+
 int main(void){
 	ll n, MAX_W, ans = 1;
 	scanf("%lld %lld", &n, &MAX_W);
-	unordered_set<string> ss;
-	ll* mm = new ll[MAX_N];
-	for(int i = 0; i <= n; i++) mm[i] = -1;
+	node* mm = new node[MAX_N];
+	edge* ee = new edge[MAX_N];
 	for(int i = 1; i < n; i++){
-		ll t1, t2, tw;
-		scanf("%lld %lld %lld", &t1, &t2, &tw);
-		ss.insert(to_string(min(t1, t2)) + "-" + to_string(max(t1, t2)));
-		if(tw > mm[t1]) mm[t1] = tw;
-		if(tw > mm[t2]) mm[t2] = tw;
+		ll t1, t2;
+		scanf("%lld %lld %lld", &t1, &t2, &ee[i].w);
+		ee[i].n1 = min(t1, t2);
+		ee[i].n2 = max(t1, t2);
+		ee[i].w = ((ee[i].w <= MAX_W) ? ee[i].w: MAX_W);
+		if(ee[i].w > mm[ee[i].n1].w) mm[ee[i].n1].w = ee[i].w;
+		if(ee[i].w > mm[ee[i].n2].w) mm[ee[i].n2].w = ee[i].w;
+		mm[ee[i].n2].i = ee[i].n2;
+		mm[ee[i].n1].i = ee[i].n1;
 	}	
-	for(ll i = 1; i <= n; i++){
-		for(ll j = i+1; j <= n; j++){
-			if(ss.find(to_string(i) + "-" + to_string(j)) == ss.end()){
-				ll diff = MAX_W - max(mm[i], mm[j]);
-				ans = (ans * ( (MAX_W - max(mm[i], mm[j])) + 2LL) % MOD ) % MOD;
-			}
-		}
+	for(int i = 1; i < n; i++){
+		if(mm[ee[i].n1].w > mm[ee[i].n2].w) mm[ee[i].n1].a++;
+		else mm[ee[i].n2].a++;
 	}
-	printf("%lld\n", ans);
+	sort(mm+1, mm+n+1, cmp);
+	for(ll i = n; i > 0; i--){
+		ll poss = (MAX_W - mm[i].w) + 2LL;
+		ll expo = (i - 1LL - mm[i].a);
+		ans = (ans * (exp(poss, expo, MOD)) % MOD) % MOD;
+	}
+	printf("%lld\n", (ans) % MOD);
 	delete []mm;
+	delete []ee;
 	return 0;
 }
