1dba3b6a899f6e44517621adf00cf890e8fb4e86
gavinleroy <gavinleroy6@gmail.com>
2019-06-14 21:31:55 -0600

    Updated SUMAGCD

--- SUMAGCD.cpp
+++ SUMAGCD.cpp
@@ -42,6 +42,11 @@ static inline T gcd(T a, T b) {
     }
     return a;
 }
+
+template<typename T>
+static inline T abs(T a, T b){
+	return ((a-b < 0) ? b-a: a-b);
+}
  
 template<typename T>
 static inline T sqr(T a) {
@@ -80,7 +85,7 @@ static inline void print_time(const std::string &name) {
     std::cerr << name << " " << get_time() << "s." << std::endl;
 }
  
-template <class T>
+template<typename T>
 static inline T pw(T a, ll n) {
     T ret = T(1);
     while (n > 0) {
@@ -91,7 +96,7 @@ static inline T pw(T a, ll n) {
     return ret;
 }
  
-template <class T>
+template<typename T>
 static inline T pw(T a, T n, T mod) {
     T ret = T(1);
     while (n > 0) {
@@ -102,7 +107,7 @@ static inline T pw(T a, T n, T mod) {
     return ret;
 }
 
-template <class T>
+template<typename T>
 inline static T mul(T a, T b, T mod) { 
     T res = T(0); 
     a = a % mod; 
@@ -117,12 +122,56 @@ inline static T mul(T a, T b, T mod) {
 
 using namespace std;
 
+int aa[100001];
+
+int findMaxGCD(int arr[], int n, int high) { 
+	int divisors[high + 1] = { 0 }; 
+	for (int i = 0; i < n; i++) { 
+		for (int j = 1; j <= sqrt(arr[i]); j++) { 
+			if (arr[i] % j == 0) { 
+				divisors[j]++; 
+				if (j != arr[i] / j) divisors[arr[i] / j]++; 
+			} 
+		} 
+	} 
+
+	for (int i = high; i >= 1; i--){
+		if (divisors[i] > 1) return i;     
+	}
+} 
+int solve(int aa[], int n, int max){
+	int g1 = aa[0], g2 = aa[1];
+	for(int i = 2; i < n; i++){
+		int t1 = gcd(g1, aa[i]), t2 = gcd(g2, aa[i]);
+		if(t1-g1 > t2-g2){
+//			cout << "Gcd1 was: " << g1 << " will be " << t1 << endl;
+//			cout << "added : " << aa[i] << " to arr1" << endl;
+		       	g1 = t1;
+		}else{
+//			cout << "Gcd2 was: " << g2 << " will be " << t2 << endl;
+//			cout << "added : " << aa[i] << " to arr2" << endl;
+		       	g2 = t2;
+		}
+	}
+	return g1 + g2;
+}
+
 int main(void){BOOST
 	init_time();
 	#ifdef LOCAL
 		freopen("input.1", "r", stdin);
 	#endif
-		
+	int t, n, max;
+	cin >> t;
+	while(t--){
+		max = 0;
+		cin >> n;
+		for(int i = 0; i < n; i++){ 
+			cin >> aa[i];	
+			remax(max, aa[i]);
+		}
+		cout << solve(aa, n, max) << endl;
+	}	
 	print_time("Time: ");
 	return 0;
 }
