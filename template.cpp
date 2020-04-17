bb336e53c32d685a6583c4922731326860a3b145
gavinleroy <gavinleroy6@gmail.com>
2020-04-17 00:33:31 -0600

    Improved template for fstio

--- template.cpp
+++ template.cpp
@@ -17,20 +17,23 @@ template<typename T> I T abs(T a){return a>0?a:-a;}
 #define RT return *this;
 #define TR *this,x;return x;
 #define SS std::string
+#define ERR !P.E
 #ifdef _WIN32
-struct CHARG{bl err=0;I UC OP()(){RC r=getchar();err=r==EOF;return r;}};
-struct CHARP{I void OP()(char x){putchar(x);}};
+#define GETC getchar()
+#define PUTC(x) putchar(x)
 #else 
-struct CHARG{bl err=0;I UC OP()(){RC r=getchar_unlocked();err=r==EOF;return r;}};
-struct CHARP{I void OP()(char x){putchar_unlocked(x);}};
+#define GETC getchar_unlocked()
+#define PUTC(x) putchar_unlocked(x)
 #endif /* _WIN32 */
-#define RX x=0; UC t=P(); while((t<'0'||t>'9')&&t!='-'&&!P.err)t=P(); bool f=0;\
-if(t=='-')t=P(),f=1; x=t-'0';for(t=P();t>='0'&&t<='9'&&!P.err; t=P()) x=x*10+t-'0'
-#define RL if(t=='.'){lf u=0.1;for(t=P();t>='0'&&t<='9'&&!P.err;t=P(),u*=0.1)x+=u*(t-'0');}if(f)x=-x
-#define RU x=0; UC t=P(); while((t<'0'||t>'9')&&!P.err)t=P();x=t-'0';\
-for(t=P();t>='0'&&t<='9'&&!P.err;t=P())x=x*10+t-'0'
+struct CHARG{bl E=0;I UC OP()(){RC r=GETC;E=(E||r==EOF);return r;}};
+struct CHARP{I void OP()(char x){PUTC(x);}};
+#define RX x=0; UC t=P(); while((t<'0'||t>'9')&&t!='-'&&ERR)t=P(); bool f=0;\
+if(t=='-')t=P(),f=1; x=t-'0';for(t=P();t>='0'&&t<='9'&&ERR; t=P()) x=x*10+t-'0'
+#define RL if(t=='.'){lf u=0.1;for(t=P();t>='0'&&t<='9'&&ERR;t=P(),u*=0.1)x+=u*(t-'0');}if(f)x=-x
+#define RU x=0; UC t=P(); while((t<'0'||t>'9')&&ERR)t=P();x=t-'0';\
+for(t=P();t>='0'&&t<='9'&&ERR;t=P())x=x*10+t-'0'
 I bool IS(char x){ return x==10||x==13||x==' '; }
-template<typename T> struct FREAD{T P;I OP bl(){return !P.err;}I FREAD&OP,(int&x){RX; if(f)x=-x; RT }I OP int(){int x;TR}I FREAD&OP,(ll & x){RX;if(f)x=-x;RT}I OP ll(){ll x;TR}I FREAD&OP,(char & x){for(x=P();IS(x)&&!P.err;x=P());RT}I OP char(){char x;TR}I FREAD&OP,(char* x){UC t=P();for(;IS(t)&&!P.err;t=P());if(~t){for(;!IS(t)&&~t&&!P.err;t=P())*x++=t;}*x++=0;RT}I FREAD&OP,(lf&x){RX;RL;RT}I OP lf(){lf x;TR}I FREAD&OP,(llf&x){RX;RL;RT}I OP llf() {llf x;TR}I FREAD&OP,(uint&x){RU;RT}I OP uint(){uint x;TR}I FREAD&OP,(ull&x){RU;RT}I OP ull(){ull x;TR}I FREAD&OP,(ul&x){RU;RT}I OP ul(){ul x;TR}};FREAD<CHARG>in;
+template<typename T> struct FREAD{T P;I OP bl(){return ERR;}I FREAD&OP,(int&x){RX; if(f)x=-x; RT }I OP int(){int x;TR}I FREAD&OP,(ll & x){RX;if(f)x=-x;RT}I OP ll(){ll x;TR}I FREAD&OP,(char & x){for(x=P();IS(x)&&ERR;x=P());RT}I OP char(){char x;TR}I FREAD&OP,(char* x){UC t=P();for(;IS(t)&&ERR;t=P());if(~t){for(;!IS(t)&&~t&&ERR;t=P())*x++=t;}*x++=0;RT}I FREAD&OP,(lf&x){RX;RL;RT}I OP lf(){lf x;TR}I FREAD&OP,(llf&x){RX;RL;RT}I OP llf() {llf x;TR}I FREAD&OP,(uint&x){RU;RT}I OP uint(){uint x;TR}I FREAD&OP,(ull&x){RU;RT}I OP ull(){ull x;TR}I FREAD&OP,(ul&x){RU;RT}I OP ul(){ul x;TR}};FREAD<CHARG>in;
 #define WI(S) if(x){if(x<0)P('-'),x=-x;UC s[S],c=0;while(x)s[c++]=x%10+'0',x/=10;while(c--)P(s[c]);}else P('0')
 #define WL if(y){lf t=0.5;for(int i=y;i--;)t*=0.1;if(x>=0)x+=t;else x-=t,P('-');*this,(ll)(abs(x));P('.');if(x<0)\
 x=-x;while(y--){x*=10;x-=floor(x*0.1)*10;P(((int)x)%10+'0');}}else if(x>=0)*this,(ll)(x+0.5);else *this,(ll)(x-0.5);
