783282d68b8931349d8f71e99f79cc6a0fec49ba
gavinleroy <gavinleroy6@gmail.com>
2021-07-02 22:46:20 -0600

    updated template ml

--- template.ml
+++ template.ml
@@ -4,10 +4,10 @@
 (* ~~~~~~~~~~~~~~~~~~~~ IO functions ~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
 let create_reader fmt f = Scanf.(bscanf Scanning.stdin fmt f)
 let id = fun n -> n
-let read_int () = create_reader "%d" id
-let read_long () = create_reader "%Ld" id
-let read_float () = create_reader "%f" id
-let read_ipair () = create_reader "%d %d" (fun a b -> (a, b))
+let read_int _ = create_reader " %d " id
+let read_long _ = create_reader " %Ld " id
+let read_float _ = create_reader " %f " id
+let read_ipair _ = create_reader " %d %d " (fun a b -> (a, b))
 (* ~~~~~~~~~~~~~~~~~~~ helper functions ~~~~~~~~~~~~~~~~~~~~~~~ *)
 let ( ** ) = Int64.mul
 let ( ++ ) = Int64.add
