6b097f413a98db7863dbe74843f003ac22a7ce7c
Gavin Gray <gavinleroy6@gmail.com>
2021-12-06 11:17:20 +0100

    day 6

---
+++ AOC/21/6/b.scala
@@ -0,0 +1,20 @@
+// Gavin Gray AOC 21
+
+import scala.io.StdIn.readLine
+import scala.collection.immutable
+
+object Day6_b {
+  def main(args: Array[String]): Unit = {
+    val init = readLine()
+      .split(",")
+      .map(_.toInt)
+      .foldLeft(Vector[Long](0, 0, 0, 0, 0, 0, 0, 0, 0))(
+        (acc: Vector[Long], i: Int) => acc.updated(i, acc(i) + 1)
+      )
+      val ans: Long = (1 to 256).foldLeft(init)((vec, _) => {
+                 val hd +: tl = vec
+                 tl.updated(6, tl(6) + hd) :+ hd
+               }).sum
+    println(ans)
+  }
+}
