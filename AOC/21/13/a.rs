f058844216f90c1fc404575151829e8fd77095f9
Gavin Gray <gavinleroy6@gmail.com>
2021-12-13 10:15:56 +0100

    day 13

---
+++ AOC/21/13/a.rs
@@ -0,0 +1,73 @@
+// Gavin Gray AOC 2021
+
+#![feature(btree_drain_filter)]
+#![feature(stdin_forwarders)]
+use std::collections::BTreeSet;
+use std::io;
+
+type Posn = (usize, usize);
+
+enum Fold {
+    X(usize),
+    Y(usize),
+}
+
+impl Fold {
+    pub fn parse(s: &String) -> Option<Self> {
+        let (t, v) = s
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .split_once("=")
+            .unwrap();
+        let iv = v.parse::<usize>().unwrap();
+        match t {
+            "x" => Some(Fold::X(iv)),
+            "y" => Some(Fold::Y(iv)),
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn iinsert(paper: &mut BTreeSet<Posn>, p: Posn) {
+    let _ = paper.insert(p);
+}
+
+fn parse_posn(s: &String) -> Option<Posn> {
+    let (xs, ys) = s.split_once(",")?;
+    Some((xs.parse::<usize>().unwrap(), ys.parse::<usize>().unwrap()))
+}
+
+fn fold(mut paper: &mut BTreeSet<Posn>, fold: &Fold) {
+    let under: BTreeSet<Posn>;
+    match fold {
+        Fold::X(l) => {
+            under = paper
+                .drain_filter(|(x, _)| x > l)
+                .map(|(x, y)| (l - (x - l), y))
+                .collect();
+        }
+        Fold::Y(l) => {
+            under = paper
+                .drain_filter(|(_, y)| y > l)
+                .map(|(x, y)| (x, l - (y - l)))
+                .collect();
+        }
+    }
+    under.iter().for_each(|&p| iinsert(&mut paper, p));
+}
+
+fn main() {
+    let (sdots, sfolds): (Vec<String>, Vec<String>) = io::stdin()
+        .lines()
+        .map(|l| l.unwrap())
+        .partition(|l| l.len() < 14);
+    let mut paper = BTreeSet::<Posn>::default();
+    sdots
+        .iter()
+        .filter_map(parse_posn)
+        .for_each(|p| iinsert(&mut paper, p));
+    let folds: Vec<Fold> = sfolds.iter().flat_map(Fold::parse).collect();
+    fold(&mut paper, folds.first().unwrap());
+    println!("{}", paper.len())
+}
