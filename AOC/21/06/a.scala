9f6ab6434c38b2ed0a86a785d0d8e9a79f6ff1a4
Gavin Gray <gavinleroy6@gmail.com>
2021-12-26 10:40:07 +0100

    day 6

--- AOC/21/06/a.scala
+++ AOC/21/06/a.scala
@@ -3,17 +3,14 @@
 import scala.io.StdIn.readLine
 import scala.collection.immutable
 
-object A {
-  def main(args: Array[String]): Unit = {
+object A:
+  def main(args: Array[String]): Unit =
     val init = readLine()
       .split(",")
       .map(_.toInt)
-      .foldLeft(Vector[Long](0, 0, 0, 0, 0, 0, 0, 0, 0))(
+      .foldLeft(Vector.fill(9)(0L))(
         (acc: Vector[Long], i: Int) => acc.updated(i, acc(i) + 1))
-      val ans: Long = (1 to 80).foldLeft(init)((vec, _) => {
+      val ans: Long = (1 to 80).foldLeft(init)((vec, _) =>
                  val hd +: tl = vec
-                 tl.updated(6, tl(6) + hd) :+ hd
-               }).sum
+                 tl.updated(6, tl(6) + hd) :+ hd).sum
     println(ans)
-  }
-}
--- AOC/21/06/b.scala
+++ AOC/21/06/b.scala
@@ -3,17 +3,14 @@
 import scala.io.StdIn.readLine
 import scala.collection.immutable
 
-object B {
-  def main(args: Array[String]): Unit = {
+object B:
+  def main(args: Array[String]): Unit =
     val init = readLine()
       .split(",")
       .map(_.toInt)
-      .foldLeft(Vector[Long](0, 0, 0, 0, 0, 0, 0, 0, 0))(
+      .foldLeft(Vector.fill(9)(0L))(
         (acc: Vector[Long], i: Int) => acc.updated(i, acc(i) + 1))
-      val ans: Long = (1 to 256).foldLeft(init)((vec, _) => {
+      val ans: Long = (1 to 256).foldLeft(init)((vec, _) =>
                  val hd +: tl = vec
-                 tl.updated(6, tl(6) + hd) :+ hd
-               }).sum
+                 tl.updated(6, tl(6) + hd) :+ hd).sum
     println(ans)
-  }
-}
