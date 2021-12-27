559a58426c94aadb079769c4479f6bfd866bc767
Gavin Gray <gavinleroy6@gmail.com>
2021-12-27 11:37:32 +0100

    day 1

--- AOC/21/01/a.ml
+++ AOC/21/01/a.ml
@@ -1,43 +1,26 @@
 (* Gavin Gray AOC 21 *)
 
-(* ~~~~~~~~~~~~~~~~~~~~ IO functions ~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
-let create_reader fmt f = Scanf.(bscanf Scanning.stdin fmt f)
-let id = fun n -> n
-let read_int _ = create_reader " %d " id
-let read_long _ = create_reader " %Ld " id
-let read_float _ = create_reader " %f " id
-let read_ipair _ = create_reader " %d %d " (fun a b -> (a, b))
-(* ~~~~~~~~~~~~~~~~~~~ helper functions ~~~~~~~~~~~~~~~~~~~~~~~ *)
-let ( ** ) = Int64.mul
-let ( ++ ) = Int64.add
-let ( -- ) = Int64.sub
-let ( // ) = Int64.div
-let ( %% ) = Int64.rem
-let curry f = fun a b -> f (a, b)
-let uncurry f = fun (a, b) -> f a b
-(* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
+let rec full_inp () =
+  let get_int () =
+    (try Some
+           (input_line stdin)
+     with _ -> None)
+    |> function
+    | (Some s) -> int_of_string_opt s
+    | None  -> None
+  in
+  match get_int () with
+  | Some i -> i :: full_inp ()
+  | None -> []
 
-exception Impossible
-
-let get_int () =
-  (try Some
-         (input_line stdin)
-   with _ -> None)
-  |> function
-  | (Some s) -> int_of_string_opt s
-  | None  -> None
-
-let rec fold_ans v o1 o2 = match o1, o2 with
-  | (Some p), (Some c) ->
-    get_int ()
-    |> fold_ans (if p < c then
-                   v + 1
-                 else v) o2
-  | _, None -> v
-  | _ -> raise Impossible
+let rec fold_ans v ns = match ns with
+  | n1 :: n2 :: tl ->
+    fold_ans (if n1 < n2 then
+                v + 1
+              else v) (n2 :: tl)
+  | _ -> v
 
 let () =
-  let f = get_int ()
-  and s = get_int () in
-  fold_ans 0 f s
-  |> print_int
+  let nums = full_inp () in
+  fold_ans 0 nums
+  |> Printf.printf "%d\n"
--- AOC/21/01/b.ml
+++ AOC/21/01/b.ml
@@ -1,48 +1,26 @@
 (* Gavin Gray AOC 21 *)
 
-(* ~~~~~~~~~~~~~~~~~~~~ IO functions ~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
-let create_reader fmt f = Scanf.(bscanf Scanning.stdin fmt f)
-let id = fun n -> n
-let read_int _ = create_reader " %d " id
-let read_long _ = create_reader " %Ld " id
-let read_float _ = create_reader " %f " id
-let read_ipair _ = create_reader " %d %d " (fun a b -> (a, b))
-(* ~~~~~~~~~~~~~~~~~~~ helper functions ~~~~~~~~~~~~~~~~~~~~~~~ *)
-let ( ** ) = Int64.mul
-let ( ++ ) = Int64.add
-let ( -- ) = Int64.sub
-let ( // ) = Int64.div
-let ( %% ) = Int64.rem
-let curry f = fun a b -> f (a, b)
-let uncurry f = fun (a, b) -> f a b
-(* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *)
+let rec full_inp () =
+  let get_int () =
+    (try Some
+           (input_line stdin)
+     with _ -> None)
+    |> function
+    | (Some s) -> int_of_string_opt s
+    | None  -> None
+  in
+  match get_int () with
+  | Some i -> i :: full_inp ()
+  | None -> []
 
-exception Impossible
-
-let get_int () =
-  (try Some (input_line stdin)
-   with _ -> None)
-  |> function
-  | (Some s) -> int_of_string_opt s
-  | None  -> None
-
-let rec fold_ans v o1 o2 o3 o4 =
-  match o1, o2, o3, o4 with
-  | (Some c1), (Some _), (Some _), (Some c4) ->
-    fold_ans (v + pt c1 c4)
-      o2 o3 o4 (get_int ())
-  | _, _, _, None -> v
-  | _ -> raise Impossible
-
-and pt v1 v4 =
-  if v1 < v4 then
-    1
-  else 0
+let rec fold_ans v ns = match ns with
+  | n1 :: n2 :: n3 :: n4 :: tl ->
+    fold_ans (if n1 < n4 then
+                v + 1
+              else v) (n2 :: n3 :: n4 :: tl)
+  | _ -> v
 
 let () =
-  let f = get_int ()
-  and s = get_int ()
-  and t = get_int ()
-  and l = get_int () in
-  fold_ans 0 f s t l
-  |> print_int
+  let nums = full_inp () in
+  fold_ans 0 nums
+  |> Printf.printf "%d\n"
