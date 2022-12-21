adbdd180d5243243508fbe4ebafc2b7c1ad71d55
Gavin Gray <gavinleroy6@gmail.com>
2022-12-21 23:38:26 +0100

    14

---
+++ AOC/22/14/a.ml
@@ -0,0 +1,105 @@
+(* Gavin Gray, AOC 2022 Day 14 *)
+
+module Posn = struct
+  type t = (int * int)
+  let compare (a, b) (x, y) =
+    let c = Int.compare a x in
+    if c = 0 then
+      Int.compare b y
+    else c
+  let fst (a, _) = a
+  let snd (_, b) = b
+  let down (a, b) = (a, b + 1)
+  let diag_left (a, b) = (a - 1, b + 1)
+  let diag_right (a, b) = (a + 1, b + 1)
+  let print (a, b) =
+    Printf.printf "(%d %d)" a b
+end
+
+module S = Set.Make(Posn)
+
+let read_lines name : string list =
+  let ic = open_in name in
+  let try_read () =
+    try Some (input_line ic) with End_of_file -> None in
+  let rec loop acc = match try_read () with
+    | Some s -> loop (s :: acc)
+    | None -> close_in ic; List.rev acc in
+  loop []
+
+(* Gross, lots of weird parsing *)
+let parse lines : S.t =
+  (* fill between two points *)
+  let fill_between s (a, b) (a', b') =
+    let rec fill mk beg ed =
+      List.init (ed - beg + 1) (fun i -> beg + i)
+      |> List.fold_left (fun acc v -> S.add (mk v) acc) s
+    in
+    (* fixed col, row fill *)
+    if a = a' then
+      let start = min b b' in
+      let ed = max b b' in
+      fill (fun v -> a, v) start ed
+      (* fixed row, column fill *)
+    else if b = b' then
+      let start = min a a' in
+      let ed = max a a' in
+      fill (fun v -> v, b) start ed
+    else failwith "diagonal line unexpected"
+  in
+  (* process a single row *)
+  let rec row s = function
+    | [] | [_] -> s
+    | p1 :: _arrow :: p2 :: ls' -> begin
+        let destructure s =
+          String.split_on_char ',' s |> List.map int_of_string
+        in
+        let [a; b] = destructure p1 in
+        let [x; y] = destructure p2 in
+        let s' = fill_between s (a, b) (x, y) in
+        row s' (p2 :: ls')
+      end
+  in
+  (* process each line *)
+  let rec inner s = function
+    | [] -> s
+    | l :: ls' ->
+      let s' =
+        String.split_on_char ' ' l
+        |> row s
+      in
+      inner s' ls'
+  in inner S.empty lines
+
+let solve filled =
+  let initial_posn = (500, 0) in
+  let max_row = S.fold (fun pn m -> Posn.snd pn |> max m) filled Int.min_int in
+  let max_col = S.fold (fun pn m -> Posn.fst pn |> max m) filled Int.min_int in
+  let min_col = S.fold (fun pn m -> Posn.fst pn |> min m) filled Int.max_int in
+  let rec drop_sand curr_p s =
+    let r = Posn.snd curr_p in
+    let c = Posn.fst curr_p in
+    let down = Posn.down curr_p in
+    let left = Posn.diag_left curr_p in
+    let right = Posn.diag_right curr_p in
+    if max_row < r || c < min_col || max_col < c then
+      None
+    else if not(S.mem down s) then
+      drop_sand down s
+    else if not(S.mem left s) then
+      drop_sand left s
+    else if not(S.mem right s) then
+      drop_sand right s
+    else
+      let s' = S.add curr_p s in
+      Some (s', s')
+  in
+  Seq.unfold (drop_sand initial_posn) filled
+  |> List.of_seq
+  |> List.length
+
+let () =
+  read_lines "input.text"
+  |> parse
+  |> solve
+  |> Printf.printf "%d\n"
--- AOC/22/README.md
+++ AOC/22/README.md
@@ -28,6 +28,7 @@
 | 11  | <p><a href="https://racket-lang.org/">Racket</a></p> |
 | 12  | <p><a href="https://racket-lang.org/">Racket</a></p> |
 | 13  | <p><a href="https://racket-lang.org/">Racket</a></p> |
+| 14  | <p><a href="https://ocaml.org/">OCaml</a></p>        |
 
 </td><td>
 
